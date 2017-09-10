5f0811e578d58ec7f90f4266c7c29b1dab0cef90
Virender <vthakur.dev@gmail.com>
2017-09-10 17:33:16 +0530

    NSE updted

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,23 @@
 Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 Route::get('/','DashboardContoller@index');
 
+// Route::get('import',function()
+// {
+//   $csv = array_map('str_getcsv', file('ind_nifty500list.csv'));
+//   foreach($csv as $getCsv)
+//   {
+//     $companyData = \App\Company::where('nse_code', $getCsv[2])->first();
+//     if($companyData)
+//     {
+//       $companyData->industry = $getCsv[1];
+//       $companyData->nsc_500 = 1;
+//       $companyData->save();
+//     }
+//   }
+// });
+
 Route::group([ 'middleware'=>'auth'],function()
 {
-
   Route::get('portfolio/future', 'PortfolioController@index');
   Route::post('portfolio/future', 'PortfolioController@index');
 
@@ -14,8 +28,7 @@ Route::group([ 'middleware'=>'auth'],function()
   Route::post('portfolio/future/store', 'PortfolioController@futureStore');
   Route::resource('portfolio', 'PortfolioController');
 
-
-  Route::get('getdata', 'DashboardContoller@getdata');
+//  Route::get('getdata', 'DashboardContoller@getdata');
   Route::get('manage','ManagecompanyController@manage');
   Route::get('company/edit/{companyId}', 'ManagecompanyController@getEditCompany');
   Route::post('company/edit', 'ManagecompanyController@postEditCompany');
